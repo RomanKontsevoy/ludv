e592c02cf2f92cae8afc5a6d13e57a80c456d57e
Roman Kontsevoy <3844320@gmail.com>
2019-01-30 16:06:53 +0200

    modified third

--- js/main.js
+++ js/main.js
@@ -1,6 +1,43 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        showWindow: false
+(function() {
+    var app = new Vue({
+        el: '#app',
+        data: {
+            showWindow: false
+        },
+        methods: {
+            closePopup: function(e) {
+                let el = e.target;
+                console.dir(e);
+                let inside = el.closest('#impressum') || el.closest('#impressum_btn');
+                if (!inside) {
+                    this.showWindow = false;
+                }
+            }
+        }
+    });
+
+    // проверяем поддержку
+    if (!Element.prototype.closest) {
+
+        // реализуем
+        Element.prototype.closest = function(css) {
+            var node = this;
+
+            while (node) {
+                if (node.matches(css)) return node;
+                else node = node.parentElement;
+            }
+            return null;
+        };
     }
-});
+
+    if (!Element.prototype.matches) {
+
+        // определяем свойство
+        Element.prototype.matches = Element.prototype.matchesSelector ||
+            Element.prototype.webkitMatchesSelector ||
+            Element.prototype.mozMatchesSelector ||
+            Element.prototype.msMatchesSelector;
+
+    }
+})();
